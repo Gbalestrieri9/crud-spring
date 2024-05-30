cf207604659bac56806ad0847656c417fb3eb0da
gabriel barros <gabrielbalestrieri@hotmail.com>
2024-05-30 11:17:51 -0300

    Novo EndPoint: FindByID

--- src/main/java/com/angularspring/crudspring/controller/CourseController.java
+++ src/main/java/com/angularspring/crudspring/controller/CourseController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angularspring.crudspring.model.Course;
@@ -28,10 +30,18 @@ public class CourseController {
     public List<Course> list() {
         return courseRepository.findAll();
     }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findByID (@PathVariable Long id){
+    	return courseRepository.findById(id)
+    			.map(record -> ResponseEntity.ok().body(record))
+    			.orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    @ResponseStatus(code= HttpStatus.CREATED )
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 
 }
